c04ef1bbfa648a6a827ca6d532f7b57cb40a0e73
Satyam Patel <satyamcloud623@gmail.com>
2021-07-23 16:08:57 -0400

    inital commit for sns topic module

---
+++ main.tf
@@ -0,0 +1,7 @@
+
+    provider "aws" {
+        region     = "us-east-1"
+        access_key = "AKIA6H75NSOYIU7VZWEU"
+        secret_key = "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"
+    }
+         
